31dedbcf0c0f7c12296fb54978c1d934e8336289
Bonard Alfin <156160403+bonardalfin@users.noreply.github.com>
2025-07-18 16:19:31 +0700

    Update PotentiometerControl.h

--- Code/_openMIDIStomper/PotentiometerControl.h
+++ Code/_openMIDIStomper/PotentiometerControl.h
@@ -1,14 +1,15 @@
 #ifndef POTENTIOMETERCONTROL_H
 #define POTENTIOMETERCONTROL_H
 
-#include <ResponsiveAnalogRead.h>  // https://github.com/dxinteractive/ResponsiveAnalogRead
+#include <MIDIUSB.h>                    // Add this to send MIDI
+#include <ResponsiveAnalogRead.h>      // https://github.com/dxinteractive/ResponsiveAnalogRead
 
 // Potentiometers
-const int N_POTS = 2;                             //* total numbers of pots (slide & rotary)
-const int POT_ARDUINO_PIN[N_POTS] = { A10, A0 };  //* pins of each pot connected straight to the Arduino
+const int N_POTS = 1;                                 //* total numbers of pots (slide & rotary). Default = 2;
+const int POT_ARDUINO_PIN[N_POTS] = { 10 };       //* pins of each pot connected straight to the Arduino. Default = { 10, A0}
 
 // Responsive Analog Read
-float snapMultiplier = 0.01;                      // (0.0 - 1.0) - Increase for faster, but less smooth reading
+float snapMultiplier = 0.01;                       // (0.0 - 1.0) - Increase for faster, but less smooth reading
 ResponsiveAnalogRead responsivePot[N_POTS] = {};  // creates an array for the responsive pots. It gets filled in the Setup.
 
 // Potentiometer states
@@ -32,7 +33,7 @@ void potentiometers() {
     int reading = analogRead(POT_ARDUINO_PIN[i]);
     responsivePot[i].update(reading);
     potCState[i] = responsivePot[i].getValue();         // Use responsive analog read value
-    midiCState[i] = map(potCState[i], 0, 1023, 0, 127);  // Maps the reading of the potCState to a value usable in MIDI
+ midiCState[i] = map(potCState[i], 0, 1023, 0, 127);  // Maps the reading of the potCState to a value usable in MIDI
     int potVar = abs(potCState[i] - potPState[i]);      // Calculates the absolute value between the difference between the current and previous state of the pot
 
     if (potVar > varThreshold) {  // Opens the gate if the potentiometer variation is greater than the threshold
@@ -40,25 +41,25 @@ void potentiometers() {
     }
 
     timer[i] = millis() - PTime[i];  // Resets the timer
+    potMoving = (timer[i] < TIMEOUT);
 
-    if (timer[i] < TIMEOUT) {  // If the timer is less than the maximum allowed time, the potentiometer is still moving
-      potMoving = true;
-    } else {
-      potMoving = false;
-    }
-
-    if (potMoving == true) {  // If the potentiometer is still moving, send the change control
-      if (midiPState[i] != midiCState[i]) {
+    if (potMoving && midiPState[i] != midiCState[i]) {
 
 #ifdef DEBUG
-        Serial.print("Pot: ");
-        Serial.print(i);
-        Serial.print(" ");
-        Serial.println(midiCState[i]);
+      Serial.print("Pot: ");
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.println(midiCState[i]);
 #endif
-        potPState[i] = potCState[i];  // Stores the current reading of the potentiometer to compare with the next
-        midiPState[i] = midiCState[i];
-      }
+
+      // --- Send MIDI CC ---
+      midiEventPacket_t event = {0x0B, 0xB0 | (1 - 1), i, midiCState[i]};
+      MidiUSB.sendMIDI(event);
+      MidiUSB.flush();
+      // ----------------------
+
+      potPState[i] = potCState[i];
+      midiPState[i] = midiCState[i];
     }
   }
 }
